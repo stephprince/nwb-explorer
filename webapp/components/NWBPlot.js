70c37725b8799940a214aa35c3a543eb4be0aa1e
Steph Prince <stephprince@users.noreply.github.com>
2021-07-01 09:43:35 -0400

    add spatial series plotting options

--- webapp/components/NWBPlot.js
+++ webapp/components/NWBPlot.js
@@ -42,22 +42,49 @@ export default class NWBTimeseriesPlotComponent extends React.Component {
     return legendName
   }
 
+  getInstanceData (instanceY, instanceX, lineOptions, mode) {
+    let legendName = this.extractLegendName(instanceY);
+    let instanceXValues = instanceX.getTimeSeries()
+    let instanceYValues = instanceY.getTimeSeries()
+
+    // in case of 2D spatial series, plot data values against each other
+    const TimeSeriesType = instanceY.getVariable().getParent().getName() // TODO - this should probably happen elsewhere but leaving here for now
+    if (TimeSeriesType == 'SpatialSeries') {
+      if (Array.isArray(instanceYValues[0])) { // could also do something like instanceYValues.length == 2 but not good for case of array length 2
+        instanceXValues = instanceYValues[0]
+        instanceYValues = instanceYValues[1]
+      }
+    }
+
+    const trace = {
+      ...this.getSinglePlotConfiguration(lineOptions, mode),
+      x: instanceXValues,
+      y: instanceYValues,
+      path: instanceY.getInstancePath(),
+      name: legendName,
+    };
+
+    return trace;
+  }
+
   render () {
     const { instancePaths, key } = this.props;
 
 
-    const plots = instancePaths.map(instancePath => ({ 
-      x: `${instancePath}.timestamps`,
+    const plots = instancePaths.map(instancePath => ({
+      x: `${instancePath}.timestamps`, // TODO - fix this so that accurately maps unit values in both time series and spatial series cases
       y: `${instancePath}.data`,
       lineOptions: { color: this.props.modelSettings[instancePath].color }
     }));
-    
+    console.log('Plots:', plots)
+
 
     return (
       <PlotComponent
         id={key}
         plots={plots}
         extractLegendName={this.extractLegendName}
+        getInstanceData={this.getInstanceData}
       />
     )
   }
